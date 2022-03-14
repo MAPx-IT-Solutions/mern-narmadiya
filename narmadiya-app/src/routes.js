cf49de75107b610414b4a29668a8c09c5b469453
thecoder921 <54221295+thecoder921@users.noreply.github.com>
2022-03-14 22:23:06 +0530

    register form complete

--- narmadiya-app/src/routes.js
+++ narmadiya-app/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes,Route } from 'react-router';
 import { Home } from './Pages/Home';
 import Register from './Pages/Register'; 
+import {Login} from './Pages/Login'; 
 import {Patrika} from './Pages/Patrika'
 import { Samajvistar } from './Pages/Samajvistar';
 const Router = () =>{
@@ -10,6 +11,7 @@ const Router = () =>{
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/register" element={<Register/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="/patrika" element={<Patrika/>} />
             <Route path="/samajvistar" element={<Samajvistar/>} />
             {/* <Route path="*" element={<NoPage />} /> */}
